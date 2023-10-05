8e03c35c193d896cc30e15f8ef091e37087c2d2f
Zarif Karimi <zarif.karimi.1373@gmail.com>
2023-10-04 19:56:02 -0500

    adding Maven Plugin

--- src/test/java/tek/bdd/runner/TestRunner.java
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "classpath:features",
         glue = "tek.bdd.steps",
-        tags = "@US_16",
+        tags = "@Regression",
         dryRun = false, //if true wont run steps just check for un-implemented steps
         plugin = {
                 "html:target/html_report/report.html"
